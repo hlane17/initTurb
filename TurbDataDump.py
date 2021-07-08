e5228eb2296320db11a11326835bd867a9510066
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-07-08 14:13:53 -0400

    Add files via upload

--- TurbDataDump.py
+++ TurbDataDump.py
@@ -12,11 +12,12 @@ import load_from_snapshot
 import os
 
 
-# In[ ]:
+# In[17]:
 
 
 fileName = "initTurb_stirring_0.25_0.5_10" 
 datafolder = "/scratch1/08056/hlane17/initTurb/" + fileName + "/output"
+#datafolder = "/home/hlane/scpData/initTurb_stirring_0.25_0.5"
 snapNum = 751
 vRMSFinal = []
 timeFinal = []
@@ -30,7 +31,9 @@ for i in range(snapNum): #This is for taking the FINAL Mach and Fraction#.
     if (i>=10): ext='0'+str(i)                                     #This resolves naming issues
     if (i>=100): ext=str(i)
     f = h5py.File(datafolder + "/snapshot_" + ext + ".hdf5", "r")  #opens file
-    velGrad = np.array(f["PartType0"]["Velocities"])
+    ids = np.array(F["PartType0"]["ParticleIDs"])
+    
+    velGrad = np.array(f["PartType0"]["Velocities"]["IDs"])
     
     vRMSFinal.append(np.sqrt(np.sum(velGrad**2)/len(velGrad)))
 
@@ -39,17 +42,15 @@ for i in range(snapNum): #This is for taking the FINAL Mach and Fraction#.
     timeFinal.append(time)
 
 
-    positionsX = np.array(f["PartType0"]["Coordinates"])[:,0]
-    positionsY = np.array(f["PartType0"]["Coordinates"])[:,1]
-    positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2]
+    positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][ids <= 10**5]
+    positionsY = np.array(f["PartType0"]["Coordinates"])[:,1][ids <= 10**5]
+    positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2][ids <= 10**5]
     mass = np.array(f["PartType0"]["Masses"])
     centerMassX.append(np.sum(mass*positionsX)/np.sum(mass))
     centerMassY.append(np.sum(mass*positionsY)/np.sum(mass))
     centerMassZ.append(np.sum(mass*positionsZ)/np.sum(mass))
-    distances = (f["PartType0"]["Coordinates"])
-    positionsX = np.array(f["PartType0"]["Coordinates"])[:,0]
-    positionsY = np.array(f["PartType0"]["Coordinates"])[:,1]
-    positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2]
+    distances = (f["PartType0"]["Coordinates"])[ids <= 10**5]
+
     #print(len(np.array(f["PartType0"]["Masses"])))
     mass = np.array(f["PartType0"]["Masses"])
     cmassX = np.sum(mass*positionsX)/np.sum(mass)
@@ -64,7 +65,7 @@ for i in range(snapNum): #This is for taking the FINAL Mach and Fraction#.
     distance = np.sqrt((distX**2) + (distY**2) + (distZ**2))
     #medianDist.append(np.median(distance))
     rmsDist.append(np.sqrt(np.sum((distance)**2)/len(distance)))
-    dx = np.array(f["PartType0"]["Coordinates"]) - np.array([cmassX, cmassY, cmassZ]) # vector from COM
+    dx = np.array(f["PartType0"]["Coordinates"])[ids <= 10**5] - np.array([cmassX, cmassY, cmassZ]) # vector from COM
     distances = np.sqrt((dx*dx).sum(1)) # computes the distances sqrt(dX^2 + dY^2 + dZ^2)
     medianDist.append(np.median(distances))
 
@@ -72,4 +73,10 @@ for i in range(snapNum): #This is for taking the FINAL Mach and Fraction#.
 np.savetxt(str(fileName) + ".dat", np.c_[vRMSFinal, timeFinal, centerMassX, centerMassY, centerMassZ, rmsDist, medianDist,], 
            header = "#(0) vRMS (1) time (2) center mass X (3) center mass Y (4) center mass Z (5) rms distance (6) median distance"
 )
+
+
+# In[ ]:
+
+
+
 
