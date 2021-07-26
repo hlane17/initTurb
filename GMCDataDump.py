689a74b4d80012f5b505c8b9274ebee0bf6305b3
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-07-26 14:59:19 -0400

    Add files via upload

--- GMCDataDump.py
+++ GMCDataDump.py
@@ -14,17 +14,18 @@ filenames = ["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_1
  
 
 # initialize lists to store all the stuff we will want in the final data file
-
+time = []
+massDensity10 = []
+KEs = []
+MEs = []
+GEs = []
+rmsDistCOM = []
+medianDistCOM = []
 magConstant = 5.88 * 10**24
 for filename in filenames: 
     for dir in glob(sims_dir+ filename + "/output"): # this will work without having to update the list of parameters in the script - just looks for all directories that match the pattern and have an output directory inside
-        time = []
-        massDensity10 = []
-        KEs = []
-        MEs = []
-        GEs = []
-        rmsDistCOM = []
-        medianDistCOM = []
+
+
         for i in range(751):
             ext='00'+str(i);
             if (i>=10): ext='0'+str(i)                                     #This resolves naming issues
@@ -33,26 +34,26 @@ for filename in filenames:
             print("snap_" + str(i))
             f = h5py.File(current_snap, "r")  #opens file
             n = 29.9 * np.array(f["PartType0"]["Density"])
-            filter = (n>10)
-            density = np.array(f["PartType0"]["Density"])[filter]
+            ids = (n>10)
+            density = np.array(f["PartType0"]["Density"])[ids]
             time.append(load_from_snapshot.load_from_snapshot("Time",0,dir,i))
-            masses = np.array(f["PartType0"]["Masses"])[filter]
+            masses = np.array(f["PartType0"]["Masses"])[ids]
             massDensity10.append(np.sum(masses))
-            velocities = np.array(f(["PartType0"]["Velocities"]))[filter]
+            velocities = np.array(f(["PartType0"]["Velocities"]))[ids]
             kineticEnergy = np.sum(0.5 * (masses[:,None]) * (velocities ** 2)) #calculates kinetic energy of the system
             KEs.append(kineticEnergy)    
             Volumes = masses * density
-            MagneticEnergy = np.sum(Volumes*(((np.array(f["PartType0"]["MagneticField"])[:,0][filter])**2 + (np.array(f["PartType0"]["MagneticField"])[:,1][filter])**2 + (np.array(f["PartType0"]["MagneticField"])[:,2][filter])**2) * magConstant))
+            MagneticEnergy = np.sum(Volumes*(((np.array(f["PartType0"]["MagneticField"])[:,0][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,1][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,2][ids])**2) * magConstant))
             MEs.append(MagneticEnergy)
         
-            GPE = 0.5 * np.sum(np.array(f(["PartType0"]["Potential"]))[filter] * masses)
+            GPE = 0.5 * np.sum(np.array(f(["PartType0"]["Potential"]))[ids] * masses)
             GEs.append(GPE)
         
-            positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][filter]
-            positionsY = np.array(f["PartType0"]["Coordinates"])[:,1][filter]
-            positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2][filter]
+            positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][ids]
+            positionsY = np.array(f["PartType0"]["Coordinates"])[:,1][ids]
+            positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2][ids]
 
-            distances = (f["PartType0"]["Coordinates"])[filter]
+            distances = (f["PartType0"]["Coordinates"])[ids]
 
             cmassX = np.sum(masses*positionsX)/np.sum(masses)
             cmassY = np.sum(masses*positionsY)/np.sum(masses)
@@ -63,14 +64,14 @@ for filename in filenames:
             distZ = distances[:,2]-cmassZ
             distance = np.sqrt((distX**2) + (distY**2) + (distZ**2))
             rmsDistCOM.append(np.sqrt(np.sum((distance)**2)/len(distance)))
-            dx = np.array(f["PartType0"]["Coordinates"])[filter] - np.array([cmassX, cmassY, cmassZ]) # vector from COM
+            dx = np.array(f["PartType0"]["Coordinates"])[ids] - np.array([cmassX, cmassY, cmassZ]) # vector from COM
             distances = np.sqrt((dx*dx).sum(1)) # computes the distances sqrt(dX^2 + dY^2 + dZ^2)
             medianDistCOM.append(np.median(distances))
             f.close()
  
-        np.savetxt("GMC_" + filename + ".dat", np.c_[time, massDensity10, KEs, MEs, GEs, rmsDistCOM, medianDistCOM], 
-                   header = "#(0) time (1) mDensity10 (2) kinetic energy (3) magnetic energy (4) gravitational energy (5) rmsDistCOM (6) medianDistCOM"
-        )
+np.savetxt("GMC_" + filename + ".dat", np.c_[time, massDensity10, KEs, MEs, GEs, rmsDistCOM, medianDistCOM], 
+           header = "#(0) time (1) mDensity10 (2) kinetic energy (3) magnetic energy (4) gravitational energy (5) rmsDistCOM (6) medianDistCOM"
+)
 
 
 # In[ ]:
