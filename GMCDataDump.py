e8637c6cf527b0791526e5f2871729b27b7a7fc0
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-08-03 18:22:59 -0400

    Add files via upload

--- GMCDataDump.py
+++ GMCDataDump.py
@@ -9,9 +9,10 @@ from glob import glob
 import numpy as np
 from os.path import isdir
 import load_from_snapshot
+from sys import argv
 sims_dir = "/scratch1/08056/hlane17/GMCTurb/"
-filenames = ["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.01", "a_800_0.5_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.1", "a_800_1_10_1e5_y_1_0.01", "a_800_1_10_2e7_y_1_1", "a_800_1_10_2e6_y_1_0.01", "a_800_1_10_2e7_y_2_0.01", "a_800_2_10_2e7_y_1_0.01",]    
-
+#filenames = ["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.01", "a_800_0.5_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.1", "a_800_1_10_1e5_y_1_0.01", "a_800_1_10_2e7_y_1_1", "a_800_1_10_2e6_y_1_0.01", "a_800_1_10_2e7_y_2_0.01", "a_800_2_10_2e7_y_1_0.01",]    
+filenames = argv[1:]
 # initialize lists to store all the stuff we will want in the final data file
 
 magConstant = 5.88 * 10**24
