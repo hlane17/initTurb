4b86e067655b6ab7ade8bdde9601b6250b278018
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-08-03 15:29:41 -0400

    Add files via upload

--- GMCDataDump.py
+++ GMCDataDump.py
@@ -9,21 +9,34 @@ from glob import glob
 import numpy as np
 from os.path import isdir
 import load_from_snapshot
-import os
-from os import system, mkdir, chdir
-import multiprocessing 
-from multiprocessing import Pool
-# In[8]:
+sims_dir = "/scratch1/08056/hlane17/GMCTurb/"
+filenames = ["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.01", "a_800_0.5_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.1", "a_800_1_10_1e5_y_1_0.01", "a_800_1_10_2e7_y_1_1", "a_800_1_10_2e6_y_1_0.01", "a_800_1_10_2e7_y_2_0.01", "a_800_2_10_2e7_y_1_0.01",]    
+
+# initialize lists to store all the stuff we will want in the final data file
+
 magConstant = 5.88 * 10**24
-
-nproc = multiprocessing.cpu_count()
-
-sims_dir = "/scratch3/08056/hlane17/GMCTurb/"
-filename = "a_1120_1_10_2e7_y_1_0.01" #["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.1", "a_800_1_10_1e5_y_1_0.01", "a_800_1_10_2e7_y_1_1", "a_800_1_10_2e6_y_1_0.01", "a_800_1_10_2e7_y_2_0.01", "a_800_2_10_2e7_y_1_0.01",]    
-
-def DataDump(sim):
-    for dir in glob(sims_dir+ filename + "/output"):
-        sim = sim.split("/")[-1]
+for filename in filenames:
+    if(filename ="a_1120_1_10_2e7_y_1_0.01"):
+        snapNum = 350
+    if(filename ="a_800_1_10_2e7_y_0.5_0.01"):
+        snapNum = 476
+    if(filename ="a_560_1_10_2e7_y_1_0.01"):
+        snapNum = 388   
+    if(filename = "a_800_1_10_2e7_y_1_0.01"):
+        snapNum = 361
+    if(filename = "a_800_1_10_2e7_y_1_0.1"):
+        snapNum = 247
+    if(filename = "a_800_1_10_1e5_y_1_0.01"):
+        snapNum = 751
+    if(filename = "a_800_1_10_2e7_y_1_1"):
+        snapNum = 43
+    if(filename = "a_800_1_10_2e6_y_1_0.01"):
+        snapNum = 751
+    if(filename = "a_800_1_10_2e7_y_2_0.01"):
+        snapNum = 257
+    if(filename = "a_800_2_10_2e7_y_1_0.01"):
+        snapNum = 751
+    for dir in glob(sims_dir+ filename + "/output"): # this will work without having to update the list of parameters in the script - just looks for all directories that match the pattern and have an output directory inside
         time = []
         massDensity10 = []
         KEs = []
@@ -31,11 +44,11 @@ def DataDump(sim):
         GEs = []
         rmsDistCOM = []
         medianDistCOM = []
-        for i in range(751):
+        for i in range(snapNum):
             ext='00'+str(i);
             if (i>=10): ext='0'+str(i)                                     #This resolves naming issues
             if (i>=100): ext=str(i)
-            current_snap = sorted(glob(dir+"/snapshot*.hdf5"))[i]            
+            current_snap = sorted(glob(dir+"/snapshot*.hdf5"))[i] # get the last snapshot
             print("snap_" + str(i))
             f = h5py.File(current_snap, "r")  #opens file
             n = 29.9 * np.array(f["PartType0"]["Density"])
@@ -44,14 +57,14 @@ def DataDump(sim):
             time.append(load_from_snapshot.load_from_snapshot("Time",0,dir,i))
             masses = np.array(f["PartType0"]["Masses"])[ids]
             massDensity10.append(np.sum(masses))
-            velocities = np.array(f(["PartType0"]["Velocities"]))[ids]
+            velocities = np.array(f["PartType0"]["Velocities"])[ids]
             kineticEnergy = np.sum(0.5 * (masses[:,None]) * (velocities ** 2)) #calculates kinetic energy of the system
             KEs.append(kineticEnergy)    
-            Volumes = masses * density
+            Volumes = masses / density
             MagneticEnergy = np.sum(Volumes*(((np.array(f["PartType0"]["MagneticField"])[:,0][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,1][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,2][ids])**2) * magConstant))
             MEs.append(MagneticEnergy)
         
-            GPE = 0.5 * np.sum(np.array(f(["PartType0"]["Potential"]))[ids] * masses)
+            GPE = 0.5 * np.sum(np.array(f["PartType0"]["Potential"])[ids] * masses)
             GEs.append(GPE)
         
             positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][ids]
@@ -75,8 +88,18 @@ def DataDump(sim):
             f.close()
  
         np.savetxt("GMC_" + filename + ".dat", np.c_[time, massDensity10, KEs, MEs, GEs, rmsDistCOM, medianDistCOM], 
-                    header = "#(0) time (1) mDensity10 (2) kinetic energy (3) magnetic energy (4) gravitational energy (5) rmsDistCOM (6) medianDistCOM"
+                   header = "#(0) time (1) mDensity10 (2) kinetic energy (3) magnetic energy (4) gravitational energy (5) rmsDistCOM (6) medianDistCOM"
         )
-dirs = sims_dir + filename
-Pool(nproc).map(DataDump, dirs)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
