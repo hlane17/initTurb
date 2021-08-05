5dafbd08537c76ed764db877726e34c4c970e764
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-08-04 21:56:49 -0400

    Update GMCDataDump.py

--- GMCDataDump.py
+++ GMCDataDump.py
@@ -12,7 +12,7 @@ import load_from_snapshot
 from sys import argv
 import multiprocessing 
 from multiprocessing import Pool
-sims_dir = "/scratch1/08056/hlane17/GMCTurb/"
+sims_dir = "/scratch3/08056/hlane17/GMCTurb/"
 #filenames = ["a_1120_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_0.5_0.01", "a_560_1_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.01", "a_800_0.5_10_2e7_y_1_0.01", "a_800_1_10_2e7_y_1_0.1", "a_800_1_10_1e5_y_1_0.01", "a_800_1_10_2e7_y_1_1", "a_800_1_10_2e6_y_1_0.01", "a_800_1_10_2e7_y_2_0.01", "a_800_2_10_2e7_y_1_0.01",]    
 filenames = argv[1:]
 # initialize lists to store all the stuff we will want in the final data file
