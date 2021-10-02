1215a4af1920fb900e4dc071cf56ee99e61e8cd6
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-10-02 15:12:55 -0400

    Update table3.py

--- table3.py
+++ table3.py
@@ -17,57 +17,53 @@ sims_dir = "/scratch3/08056/hlane17/GMCTurb/"
 snapshots = argv[1:]
 # initialize lists to store all the stuff we will want in the final data file
 
-nproc = multiprocessing.cpu_count()
 magConstant = 5.88 * 10**24
 cms = 200
 for snapshot in snapshots:
-    def Function(snapshot):
-        f = h5py.File(snapshot, 'r')
-        print(f)
+    f = h5py.File(snapshot, 'r')
+    print(f)
        
-        n = 29.9 * np.array(f["PartType0"]["Density"])      
-        ids = (n>10)
+    n = 29.9 * np.array(f["PartType0"]["Density"])      
+    ids = (n>10)
 
-        velGrad = np.array(f["PartType0"]["Velocities"])[ids]    
-        MachRMS = np.sqrt(np.sum(velGrad**2)/len(velGrad))/cms
+    velGrad = np.array(f["PartType0"]["Velocities"])[ids]    
+    MachRMS = np.sqrt(np.sum(velGrad**2)/len(velGrad))/cms
         
-        magneticEnergy = np.sum(Volumes*(((np.array(f["PartType0"]["MagneticField"])[:,0][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,1][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,2][ids])**2) * magConstant))
-        gravPotEnergy = 0.5 * np.sum(np.array(f(["PartType0"]["Potential"]))[ids] * masses)
-        mus = 0.42*np.sqrt(np.abs(gravPotEnergy)/magneticEnergy)
-        time = f["Header"].attrs["Time"]       
+    magneticEnergy = np.sum(Volumes*(((np.array(f["PartType0"]["MagneticField"])[:,0][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,1][ids])**2 + (np.array(f["PartType0"]["MagneticField"])[:,2][ids])**2) * magConstant))
+    gravPotEnergy = 0.5 * np.sum(np.array(f["PartType0"]["Potential"])[ids] * masses)
+    mus = 0.42*np.sqrt(np.abs(gravPotEnergy)/magneticEnergy)
+    time = f["Header"].attrs["Time"]       
         
-        positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][ids]
-        positionsY = np.array(f["PartType0"]["Coordinates"])[:,1][ids]
-        positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2][ids]
+    positionsX = np.array(f["PartType0"]["Coordinates"])[:,0][ids]
+    positionsY = np.array(f["PartType0"]["Coordinates"])[:,1][ids]
+    positionsZ = np.array(f["PartType0"]["Coordinates"])[:,2][ids]
 
-        distances = np.array(f["PartType0"]["Coordinates"])[ids]
-        cmassX = np.sum(masses*positionsX)/np.sum(masses)
-        cmassY = np.sum(masses*positionsY)/np.sum(masses)
-        cmassZ = np.sum(masses*positionsZ)/np.sum(masses)
+    distances = np.array(f["PartType0"]["Coordinates"])[ids]
+    cmassX = np.sum(masses*positionsX)/np.sum(masses)
+    cmassY = np.sum(masses*positionsY)/np.sum(masses)
+    cmassZ = np.sum(masses*positionsZ)/np.sum(masses)
 
-        distX = distances[:,0]-cmassX
-        distY = distances[:,1]-cmassY
-        distZ = distances[:,2]-cmassZ
-        distance = np.sqrt((distX**2) + (distY**2) + (distZ**2))
+    distX = distances[:,0]-cmassX
+    distY = distances[:,1]-cmassY
+    distZ = distances[:,2]-cmassZ
+    distance = np.sqrt((distX**2) + (distY**2) + (distZ**2))
         
         #rmsDistCom = (np.sqrt(np.sum((distance)**2)/len(distance)))
         
-        dx = np.array(f["PartType0"]["Coordinates"])[ids] - np.array([cmassX, cmassY, cmassZ]) # vector from COM
-        distancesFinal = np.sqrt((dx*dx).sum(1)) # computes the distances sqrt(dX^2 + dY^2 + dZ^2)
+    dx = np.array(f["PartType0"]["Coordinates"])[ids] - np.array([cmassX, cmassY, cmassZ]) # vector from COM
+    distancesFinal = np.sqrt((dx*dx).sum(1)) # computes the distances sqrt(dX^2 + dY^2 + dZ^2)
         
-        medianDistCom = np.median(distancesFinal)
+    medianDistCom = np.median(distancesFinal)
         
-        velocities = np.array(f["PartType0"]["Velocities"])[ids]
-        kineticEnergy = np.sum(0.5 * (masses[:,None]) * (velocities ** 2)) #calculates kinetic energy of the system
+    velocities = np.array(f["PartType0"]["Velocities"])[ids]
+    kineticEnergy = np.sum(0.5 * (masses[:,None]) * (velocities ** 2)) #calculates kinetic energy of the system
           
-        alpha = 2*kineticEnergy/(np.abs(gravPotEnergy))
+    alpha = 2*kineticEnergy/(np.abs(gravPotEnergy))
         
-        f.close()
-        return time, mus MachRMS, medianDistCOM, alpha
+    f.close()
  
-    data = Pool(nproc).map(Function,snapshot)
 
-    np.savetxt(sims_dir "/vRMS_" + snapshot + ".dat", data, 
+    np.savetxt(sims_dir + "/" + snapshot + ".dat", np.c_[time, mus MachRMS, medianDistCOM, alpha], 
                 header = "#(0) time (1) mus (2) MachRMS (3) R50 (4) alpha "
     )
 
